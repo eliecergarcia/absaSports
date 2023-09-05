fabe89063d8192e2174891e2f43938361b7f2d6a
eliecer garcia <absalonsubzero@gmail.com>
2023-09-05 13:15:12 -0600

    se modifica archivo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,14 @@ import { AppProps } from 'next/app';
 
 import '../styles/main.css';
 import '../styles/prism-a11y-dark.css';
+import { NextUIProvider } from '@nextui-org/react'
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
+}
 
 export default MyApp;
